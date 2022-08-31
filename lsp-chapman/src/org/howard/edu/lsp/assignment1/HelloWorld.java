5d8bb9dc49f4dfa00c7756b848c5e7ca4fe67f54
kesi <kesi@localhost>
2022-08-31 19:11:07 -0400

    This is my commit for assignment #1.

---
+++ lsp-chapman/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Kesi Chapman
+ * 
+ * @author kesi
+ *
+ *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
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello LSP!!!!!");
+	}
+
+}
