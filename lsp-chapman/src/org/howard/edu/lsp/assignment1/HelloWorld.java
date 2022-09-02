6ebb8527413462b7e37251ad47bddf197887c953
kesi <kesi@kesis-air.lan>
2022-09-02 00:33:29 -0400

    Security fix.

--- lsp-chapman/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ lsp-chapman/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -5,13 +5,13 @@ package org.howard.edu.lsp.assignment1;
  * 
  * @author kesi
  *
- *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
  *
  */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello LSP!!!!!");
+		
 	}
 
 }
